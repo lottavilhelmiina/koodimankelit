3d20214ef853ccc25c15a3f5bb80aa3b55ac4f8b
Juho Rantala <juho.a.rantala@tuni.fi>
2023-02-15 22:41:09 +0200

    Changed back to POST

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/calculator/CalculatorController.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/calculator/CalculatorController.java
@@ -1,6 +1,6 @@
 package fi.tuni.koodimankelit.antibiootit.calculator;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +19,7 @@ public class CalculatorController {
     }
 
     
-    @GetMapping("calculator")
+    @PostMapping("calculator")
     public DiagnosisTreatments calculateTreatments(@RequestBody @Valid Parameters parameters) {
 
         // TEST ONLY
