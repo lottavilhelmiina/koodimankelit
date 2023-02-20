72d4d7b1080d24a790cb7566d26ffd5fd3ee1875
Juho Rantala <juho.a.rantala@tuni.fi>
2023-02-20 23:53:08 +0200

    Test apikey env variable on Railway

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/AntibiootitApplication.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/AntibiootitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AntibiootitApplication {
 
 	public static void main(String[] args) {
+		System.out.println(System.getenv("apikey"));
 		SpringApplication.run(AntibiootitApplication.class, args);
 	}
 
