507f097b3e775b899df689ae7bcc72103f3726bc
Neera Kiviluoma <neeruska.kiviluoma@gmail.com>
2023-02-21 11:55:44 +0200

    Adding repo initialization

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/AntibiootitApplication.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/AntibiootitApplication.java
@@ -1,13 +1,19 @@
 package fi.tuni.koodimankelit.antibiootit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import fi.tuni.koodimankelit.antibiootit.database.DiagnoseRepository;
 
 
 @SpringBootApplication
 @EnableMongoRepositories
 public class AntibiootitApplication {
+
+	@Autowired
+    DiagnoseRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(AntibiootitApplication.class, args);
