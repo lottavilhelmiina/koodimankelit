a199fbe547fea17a921516527960b1125a6fbecb
Neera Kiviluoma <neeruska.kiviluoma@gmail.com>
2023-02-27 12:00:45 +0200

    Adding MongoTemplate

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/database/MongoDBConfig.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/database/MongoDBConfig.java
@@ -1,9 +1,11 @@
 package fi.tuni.koodimankelit.antibiootit.database;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.client.MongoClient;
@@ -25,5 +27,10 @@ public class MongoDBConfig extends AbstractMongoClientConfiguration {
     @Override
     public MongoClient mongoClient() {
         return MongoClients.create(MONGO_URL);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), getDatabaseName());
     }
 }
