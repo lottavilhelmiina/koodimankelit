370db273bc751aa76f5dea449376dd1ec9e71f1c
Eveliina <eveliina.sundberg@tuni.fi>
2023-03-01 14:11:31 +0200

    Added endpoint grouping

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/SwaggerConfig.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/SwaggerConfig.java
@@ -1,9 +1,17 @@
 package fi.tuni.koodimankelit.antibiootit.config;
 
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    
+    @Bean
+    public GroupedOpenApi customApi() {
+        return GroupedOpenApi.builder()
+                .group("Antibiootit API")
+                .packagesToScan("fi.tuni.koodimankelit.antibiootit")
+                .build();
+    }
 }
