2752304ffc25aa5b7d8f6a4ebe8fce9c9d4ae00d
Juho Rantala <juho.a.rantala@tuni.fi>
2023-03-02 18:12:01 +0200

    Fixed unit test

--- backend/antibiootit/src/test/java/fi/tuni/koodimankelit/antibiootit/AntibioticsServiceTest.java
+++ backend/antibiootit/src/test/java/fi/tuni/koodimankelit/antibiootit/AntibioticsServiceTest.java
@@ -2,6 +2,8 @@ package fi.tuni.koodimankelit.antibiootit;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,7 +25,7 @@ public class AntibioticsServiceTest {
     public void testCalculateTreatments() {
 
         // Test not implemented, used to verify that GitHub CI/CD pipeline works
-        Parameters params = new Parameters("test-id", 20.0, false, false);
+        Parameters params = new Parameters("test-id", 20.0, false, new ArrayList<>());
         var result = antibioticsService.calculateTreatments(params);
         assertNotNull(result);
     }
