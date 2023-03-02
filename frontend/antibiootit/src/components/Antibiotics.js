332263b38fb55f2a57c9b2d301d65ce08783a712
Malla <malla.nyrhinen@gmail.com>
2023-03-02 12:54:52 +0200

    Moved antibiotic state to Antibiotic.js and use it now as props in Tratment.js. Made active recipe state

--- frontend/antibiootit/src/components/Antibiotics.js
+++ frontend/antibiootit/src/components/Antibiotics.js
@@ -14,6 +14,60 @@ export default function Antibiotics() {
         state: "Vaihe 1",
         text: "Valitse ensin potilaan diagnoosi"
     });
+
+    /** State muuttuu ku valitaan tauti ja vastaavan taudin tiedot
+     * tulee bäkistä? Nyt vaan kovakoodattu. Alle 40kg = mikstuura ja yli tabletti?
+     * Bäkistä tulee json/ array mitä tulikaan, niin otetaan indexin mukaan
+     * arvo objectiin onko toissijainen vai ensisijainen?
+     * {index === 0 ? true : false} esimerkiksi, mutta vaikee tehdä esim vasta
+     * propsina, ksoka aktiivisen vaihdossa käytän sitä tuolta? tota
+     * "firstChoise"a pitää käyttää myös siihen mihin näytetään reseptinä.
+     * Mieti Miian kanssa mikä olisi hyvä.
+    */
+
+    const [antibiotic, setAntibiotic] = React.useState([
+        {
+            id: "J03.0",
+            format: "Tabletti",
+            name: "Amoksisilliini",
+            dosage: "100mg/ml",
+            dose: "2ml",
+            doseInDay: "4ml",
+            instruction: "2 krt/vrk, yht 5rk ajan",
+            firstChoise: true,
+            recipe: "Ensimmäisen vaihtoehdon resepti"
+        },
+        {
+            id: "J03.1",
+            format: "Tabletti",
+            name: "Amoksisilliini2",
+            dosage: "100mg/ml2",
+            dose: "2ml2",
+            doseInDay: "4ml2",
+            instruction: "2 krt/vrk, yht 5rk ajan2",
+            firstChoise: false,
+            recipe: "Toisen vaihtoehdon resepti"
+        }
+    ]);
+
+    const [activeRecipe, setActiveRecipe] = React.useState(antibiotic[0].recipe);
+    console.log(activeRecipe); /** Miks tulostaa kaks kertaa :( */
+
+    function toggleChoise(name) {
+        for(let i = 0; i < antibiotic.length; i++) {
+            if(antibiotic[i].name === name) {
+                setActiveRecipe(antibiotic[i].recipe)
+            }
+        }
+
+        setAntibiotic(prevAntibiotic => {
+            return prevAntibiotic.map((antibiote) => {
+                return antibiote.name === name ? 
+                {...antibiote, firstChoise: true} : {...antibiote, firstChoise: false}
+            })
+        })
+    }
+
     
     return (
         <div className="antibiotics">
@@ -24,7 +78,10 @@ export default function Antibiotics() {
                 <p>{instruction.text}</p>
             </section>
             <Form />
-            <Treatment />
+            <Treatment 
+                antibiotic={antibiotic}
+                toggleChoise={toggleChoise}
+            />
             <Recipe />
         </div>
     );
--- frontend/antibiootit/src/components/Treatment.js
+++ frontend/antibiootit/src/components/Treatment.js
@@ -1,54 +1,12 @@
 import React from "react"
 import Choise from "./Choise"
 
-export default function Treatment() {
-    /** State muuttuu ku valitaan tauti ja vastaavan taudin tiedot
-     * tulee bäkistä? Nyt vaan kovakoodattu. Alle 40kg = mikstuura ja yli tabletti?
-     * Bäkistä tulee json/ array mitä tulikaan, niin otetaan indexin mukaan
-     * arvo objectiin onko toissijainen vai ensisijainen?
-     * {index === 0 ? true : false} esimerkiksi, mutta vaikee tehdä esim vasta
-     * propsina, ksoka aktiivisen vaihdossa käytän sitä tuolta? tota
-     * "firstChoise"a pitää käyttää myös siihen mihin näytetään reseptinä.
-     * Mieti Miian kanssa mikä olisi hyvä.
-    */
-
-    const [antibiotic, setAntibiotic] = React.useState([
-        {
-            id: "J03.0",
-            format: "Tabletti",
-            name: "Amoksisilliini",
-            dosage: "100mg/ml",
-            dose: "2ml",
-            doseInDay: "4ml",
-            instruction: "2 krt/vrk, yht 5rk ajan",
-            firstChoise: true
-        },
-        {
-            id: "J03.1",
-            format: "Tabletti",
-            name: "Amoksisilliini2",
-            dosage: "100mg/ml2",
-            dose: "2ml2",
-            doseInDay: "4ml2",
-            instruction: "2 krt/vrk, yht 5rk ajan2",
-            firstChoise: false
-        }
-    ]);
-
-
-    function toggleChoise(name) {
-        setAntibiotic(prevAntibiotic => {
-            return prevAntibiotic.map((antibiote) => {
-                return antibiote.name === name ? 
-                {...antibiote, firstChoise: true} : {...antibiote, firstChoise: false}
-            })
-        })
-    }
+export default function Treatment(props) {
 
     let AntibioticElements
 
-    if(antibiotic.length > 1) {
-        AntibioticElements = antibiotic.map((antibiote, index) => 
+    if(props.antibiotic.length > 1) {
+        AntibioticElements = props.antibiotic.map((antibiote, index) => 
             <Choise
                 key={antibiote.id}
                 index={index}
@@ -58,11 +16,11 @@ export default function Treatment() {
                 doseInDay={antibiote.doseInDay}
                 instruction={antibiote.instruction}
                 choise = {antibiote.firstChoise}
-                toggleChoise={toggleChoise}
+                toggleChoise={props.toggleChoise}
             />
         )
     } else {
-        AntibioticElements = antibiotic.map((antibiote, index) => 
+        AntibioticElements = props.antibiotic.map((antibiote, index) => 
             <Choise
                 key={antibiote.id}
                 index={index}
@@ -78,7 +36,7 @@ export default function Treatment() {
 
     return (
         <div className="treatment-container">
-            <h2>{`Hoitosuositus ${antibiotic[0].format.toLowerCase()}na`}</h2>
+            <h2>{`Hoitosuositus ${props.antibiotic[0].format.toLowerCase()}na`}</h2>
             <div>
                 <div className="choise-container">
                     {AntibioticElements}
