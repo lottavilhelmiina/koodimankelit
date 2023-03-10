8c1852cb9fe8066307f9cb1c4aea05bef88768da
Eveliina <eveliina.sundberg@tuni.fi>
2023-03-10 14:30:46 +0200

    Removed licence

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/SwaggerConfig.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/SwaggerConfig.java
@@ -6,20 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
-    // TODO: Add appropriate info about API
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                 .title("Antibiotics API")
                 .version("1.0.0")
-                .license(new License().name("Apache 2.0").url("http://springdoc.org"))
                 .description("description")
                 .contact(new Contact().name("name").email("email")))
                 .addServersItem(new Server().url("http://localhost:8080"));
