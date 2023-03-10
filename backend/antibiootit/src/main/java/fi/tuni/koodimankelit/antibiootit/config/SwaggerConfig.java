5c5a758aa0e809df9cd381c689a0968d1f0d9917
Eveliina <eveliina.sundberg@tuni.fi>
2023-03-10 14:50:41 +0200

    Edited description layout

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/SwaggerConfig.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
                 .info(new Info()
                 .title("Antibiotics API")
                 .version("1.0.0")
-                .description("The Antibiotics API provides accurate and up-to-date information on appropriate antibiotic recommendations for child patients.\n\nDevelopers:\nJuho Rantala, juho.a.rantala@tuni.fi\nNeera Kiviluoma, neera.kiviluoma@tuni.fi\nEveliina Sundberg, eveliina.sundberg@tuni.fi"))
+                .description("The Antibiotics API provides accurate and up-to-date information on appropriate antibiotic recommendations for child patients.\n\n### Developers\n\nJuho Rantala, juho.a.rantala@tuni.fi\n\nNeera Kiviluoma, neera.kiviluoma@tuni.fi\n\nEveliina Sundberg, eveliina.sundberg@tuni.fi"))
                 .addServersItem(new Server().url("http://localhost:8080"));
     }    
 }
