081b412b261bb5769e904692759b2b62857732b5
Juho Rantala <juho.a.rantala@tuni.fi>
2023-03-16 16:44:32 +0200

    Removed duplicate order

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/CorsConfig.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/CorsConfig.java
@@ -3,12 +3,10 @@ package fi.tuni.koodimankelit.antibiootit.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
 import fi.tuni.koodimankelit.antibiootit.filter.CorsFilter;
 
 @Configuration
-@Order(1)
 public class CorsConfig {
 
     @Bean
