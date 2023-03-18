e45a197acdaedddbe6cef1d1873b6fe618ba617e
Eveliina <eveliina.sundberg@tuni.fi>
2023-03-18 13:54:23 +0200

    Added api key as security requirement

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/SwaggerConfig.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/config/SwaggerConfig.java
@@ -3,10 +3,17 @@ package fi.tuni.koodimankelit.antibiootit.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @Configuration
+@OpenAPIDefinition(security = {@SecurityRequirement(name = "X-API-KEY")})
+@SecurityScheme(type = SecuritySchemeType.APIKEY, name = "X-API-KEY", in = SecuritySchemeIn.HEADER)
 public class SwaggerConfig {
 
     @Bean
