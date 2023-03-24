0f20a223513e229094750ecd3a36e27dedcc0df5
Eveliina <eveliina.sundberg@tuni.fi>
2023-03-24 14:09:34 +0200

    Rounded mixture dosage result to nearest half

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/builder/MixtureBuilder.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/builder/MixtureBuilder.java
@@ -36,11 +36,18 @@ public class MixtureBuilder extends AntibioticTreatmentBuilder {
      */
     private Double calculateDosageResult() {
         double dosagePerDay = antibiotic.getDosagePerWeightPerDay() * weight;
-
         double totalDosageInDay = dosagePerDay / strength.getValue();
-        return totalDosageInDay / antibiotic.getDosesPerDay();
-    }
-    
 
-    
+        return roundToNearestHalf(totalDosageInDay / antibiotic.getDosesPerDay());
+    }
+
+    /**
+     * Rounds a value to nearest half
+     * @param value Double to round
+     * @return Double Rounded value
+     */
+    private Double roundToNearestHalf(Double value) {
+        Double roundedValue = ((int)(value * 2 + 0.5)) / 2.0;
+        return roundedValue;
+    }
 }
