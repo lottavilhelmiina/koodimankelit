c17fe8c39b76b29f7f256714523aeece48430d6e
Juho Rantala <juho.a.rantala@tuni.fi>
2023-03-28 18:54:45 +0300

    Created getAllDiagnosisInfos tests

--- backend/antibiootit/src/test/java/fi/tuni/koodimankelit/antibiootit/service/DataHandlerTest.java
+++ backend/antibiootit/src/test/java/fi/tuni/koodimankelit/antibiootit/service/DataHandlerTest.java
@@ -1,11 +1,16 @@
 package fi.tuni.koodimankelit.antibiootit.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -73,5 +78,45 @@ public class DataHandlerTest {
     public void testGetDiagnosisInfoByIdException() {
         when(repository.getDiagnosisInfoById(any())).thenReturn(Optional.empty());
         assertThrows(DiagnosisNotFoundException.class, () -> dataHandler.getDiagnosisInfoById("test"));
+    }
+
+    /**
+     * Test getAllDiagnosisInfos contains all elements and retains order
+     */
+    @Test
+    public void testGetAllDiagnosisInfos() {
+
+        List<DiagnosisInfo> diagnosisInfos = new ArrayList<>();
+        diagnosisInfos.add(new DiagnosisInfo("test1", null, null, null, false));
+        diagnosisInfos.add(new DiagnosisInfo("test2", null, null, null, false));
+        diagnosisInfos.add(new DiagnosisInfo("test3", null, null, null, false));
+
+        when(repository.getAllDiagnosisInfos()).thenReturn(diagnosisInfos);
+        List<DiagnosisInfo> result = dataHandler.getAllDiagnosisInfos();
+
+        assertEquals(3, result.size());
+
+        // Assert that service retains the order of original list. ArrayList retains insertion order
+        assertEquals(result.get(1).getId(), "test2");
+    }
+
+    /**
+     * Test getAllDiagnosisInfos handles empty results correctly
+     */
+    @Test
+    public void testGetAllDiagnosisInfosEmpty() {
+        when(repository.getAllDiagnosisInfos()).thenReturn(new ArrayList<>());
+        List<DiagnosisInfo> result = dataHandler.getAllDiagnosisInfos();
+        assertTrue(result.isEmpty());
+    }
+
+    /**
+     * Test getAllDiagnosisInfos handles null value
+     */
+    @Test
+    public void testGetAllDiagnosisInfosNull() {
+        when(repository.getAllDiagnosisInfos()).thenReturn(null);
+        List<DiagnosisInfo> result = dataHandler.getAllDiagnosisInfos();
+        assertNull(result);
     }
 }
