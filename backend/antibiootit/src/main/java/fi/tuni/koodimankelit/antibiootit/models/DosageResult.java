f40a27c1628fcaf8e00f3992057a853dfb8ae1bd
Eveliina <eveliina.sundberg@tuni.fi>
2023-03-29 09:01:28 +0300

    Added more accurate dose value

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/models/DosageResult.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/models/DosageResult.java
@@ -8,23 +8,33 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @Schema(description = "Calculated antibiotic dosage result")
 public class DosageResult {
     private final Measurement dose;
+    private final Measurement unroundedDose;
 
 
     /**
      * Default constructor
      * @param dose one-time dosage
      */
-    public DosageResult(Measurement dose) {
+    public DosageResult(Measurement dose, Measurement unroundedDose) {
         this.dose = dose;
+        this.unroundedDose = unroundedDose;
     }
 
 
     /**
-     * Returns the one-time dose
+     * Returns the rounded one-time dose
      * @return dose
      */
     public Measurement getDose() {
         return this.dose;
+    }
+
+    /**
+     * Returns the more accurate one-time dose (rounded to three decimals)
+     * @return unrounded dose
+     */
+    public Measurement getUnroundedDose() {
+        return this.unroundedDose;
     }
 
 }
