d944e3f92719476360903c0ec23844502e36b803
Miia Honkanen <miia.honkanen@tuni.fi>
2023-04-04 11:47:18 +0300

    Add Google Forms feedback form

--- frontend/antibiootit/src/components/Feedback.js
+++ frontend/antibiootit/src/components/Feedback.js
@@ -3,8 +3,15 @@ import React from "react";
 export default function Feedback() {
     return (
         <div className="feedback-container">
-            <h2>Palautekysely</h2>
-            <p>Sivulla ei vielä ole sisältöä</p>
+            <iframe 
+                src="https://docs.google.com/forms/d/e/1FAIpQLSdnDoQe0D4rqPg2T8SC1Ezm8-_Mp7PxNxVrd9be42y8RzAduA/viewform?embedded=true" 
+                width="640" 
+                height="1488" 
+                frameborder="0" 
+                marginheight="0" 
+                marginwidth="0">
+                    Ladataan…
+            </iframe>
         </div>
     )
 }
