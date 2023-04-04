4d187bf0292a453ba87e5f4d440779d6d5d7ce00
Miia Honkanen <miia.honkanen@tuni.fi>
2023-04-04 11:55:02 +0300

    Remove problematic attributes

--- frontend/antibiootit/src/components/Feedback.js
+++ frontend/antibiootit/src/components/Feedback.js
@@ -4,12 +4,10 @@ export default function Feedback() {
     return (
         <div className="feedback-container">
             <iframe 
+                title="feedback"
                 src="https://docs.google.com/forms/d/e/1FAIpQLSdnDoQe0D4rqPg2T8SC1Ezm8-_Mp7PxNxVrd9be42y8RzAduA/viewform?embedded=true" 
                 width="640" 
-                height="1488" 
-                frameborder="0" 
-                marginheight="0" 
-                marginwidth="0">
+                height="1488">
                     Ladataan…
             </iframe>
         </div>
