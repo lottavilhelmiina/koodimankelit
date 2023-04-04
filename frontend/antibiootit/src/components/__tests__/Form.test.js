cf197c2bbf048a0842834c6d6750e79d86998289
Miia Honkanen <miia.honkanen@tuni.fi>
2023-04-04 13:28:23 +0300

    Add tests for menu items & checkboxes

--- frontend/antibiootit/src/components/__tests__/Form.test.js
+++ frontend/antibiootit/src/components/__tests__/Form.test.js
@@ -3,31 +3,103 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Form from '../Form';
 
-const menuItems = [
-    {name: "Streptokokkitonsilliitti"},
-    {name: "Bronkiitti"},
-    {name: "Third item"}
+const diagnoses = [
+    {name: "Streptokokkitonsilliitti",
+     needsAntibiotics: "true",
+     checkBoxes: [
+        {id: 'EBV-001', name: 'EBV-infektio'}]},
+    {name: "Bronkiitti",
+     needsAntibiotics: "false",
+     checkBoxes: []},
+    {name: "Välikorvatulehdus",
+     needsAntibiotics: "true",
+     checkBoxes: []},
+    {name: "Avohoitopneumonia",
+     needsAntibiotics: "true",
+     checkBoxes: [
+        {id: 'MYK-001', name: 'Mykoplasmainfektio'}]}
 ]
 
+const setChosenDiagnosis = () => {
+    console.log("Checkbox test")
+}
+
+const changeInstruction = () => {
+    console.log("Checkbox test cont.")
+}
+
 test('Should render Form', async () => {
-    render(<Form diagnoses={menuItems} />);
+    render(<Form diagnoses={diagnoses} />);
 });
 
 test('Should not render checkboxes initially', async () => {
-    render(<Form diagnoses={menuItems} />);
+    render(<Form diagnoses={diagnoses} />);
     expect(screen.queryByText('Penisilliiniallergia')).toBeNull();
     expect(screen.queryByText('Samanaikainen EBV-infektio')).toBeNull();
     expect(screen.queryByText('Samanaikainen mykoplasma')).toBeNull();
 });
 
 test('Should have the correct number of diagnoses in the menu', async () => {
-    render(<Form diagnoses={menuItems} />);
+    render(<Form diagnoses={diagnoses} />);
     fireEvent.click(screen.getByText('Valitse diagnoosi'))
     await waitFor(() => screen.getAllByTestId('diagnosis-menu'))
 
     const diagnosisMenu = screen.getByTestId('diagnosis-menu');
     const childCount = diagnosisMenu.children.length;
   
-    expect(childCount).toBe(3);
-
+    expect(childCount).toBe(4);
 });
+
+test('Should render the correct checkboxes for streptokokkitonsilliitti', async () => {
+    render(<Form 
+                diagnoses={diagnoses} 
+                setChosenDiagnosis={setChosenDiagnosis} 
+                changeInstruction={changeInstruction}/>);
+    
+    fireEvent.click(screen.getByText('Valitse diagnoosi'))
+    await waitFor(() => screen.getAllByTestId('diagnosis-menu'))
+
+    const diagnosisMenu = screen.getByTestId('diagnosis-menu');
+    fireEvent.click(screen.getByText('Streptokokkitonsilliitti'))
+    await waitFor(() => {
+        expect(screen.queryByText('Penisilliiniallergia')).toBeInTheDocument();
+        expect(screen.queryByText('Samanaikainen EBV-infektio')).toBeInTheDocument();
+        expect(screen.queryByText('Samanaikainen mykoplasma')).toBeNull();
+      });
+})
+
+test('Should render the correct checkboxes for avohoitopneumonia', async () => {
+    render(<Form 
+                diagnoses={diagnoses} 
+                setChosenDiagnosis={setChosenDiagnosis} 
+                changeInstruction={changeInstruction}/>);
+    
+    fireEvent.click(screen.getByText('Valitse diagnoosi'))
+    await waitFor(() => screen.getAllByTestId('diagnosis-menu'))
+
+    const diagnosisMenu = screen.getByTestId('diagnosis-menu');
+    fireEvent.click(screen.getByText('Avohoitopneumonia'))
+    await waitFor(() => {
+        expect(screen.queryByText('Penisilliiniallergia')).toBeInTheDocument();
+        expect(screen.queryByText('Samanaikainen EBV-infektio')).toBeNull();
+        expect(screen.queryByText('Samanaikainen mykoplasma')).toBeInTheDocument();
+      });
+})
+
+test('Should render the correct checkbox for bronkiitti', async () => {
+    render(<Form 
+                diagnoses={diagnoses} 
+                setChosenDiagnosis={setChosenDiagnosis} 
+                changeInstruction={changeInstruction}/>);
+    
+    fireEvent.click(screen.getByText('Valitse diagnoosi'))
+    await waitFor(() => screen.getAllByTestId('diagnosis-menu'))
+
+    const diagnosisMenu = screen.getByTestId('diagnosis-menu');
+    fireEvent.click(screen.getByText('Bronkiitti'))
+    await waitFor(() => {
+        expect(screen.queryByText('Penisilliiniallergia')).toBeInTheDocument();
+        expect(screen.queryByText('Samanaikainen EBV-infektio')).toBeNull();
+        expect(screen.queryByText('Samanaikainen mykoplasma')).toBeNull();
+      });
+})
