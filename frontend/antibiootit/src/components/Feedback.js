709945e503835924fe8d5f858ac2aa687f26b23e
Miia Honkanen <miia.honkanen@tuni.fi>
2023-04-04 11:58:14 +0300

    Add styling to remove border

--- frontend/antibiootit/src/components/Feedback.js
+++ frontend/antibiootit/src/components/Feedback.js
@@ -5,6 +5,7 @@ export default function Feedback() {
         <div className="feedback-container">
             <iframe 
                 title="feedback"
+                className="feedback-form"
                 src="https://docs.google.com/forms/d/e/1FAIpQLSdnDoQe0D4rqPg2T8SC1Ezm8-_Mp7PxNxVrd9be42y8RzAduA/viewform?embedded=true" 
                 width="640" 
                 height="1488">
