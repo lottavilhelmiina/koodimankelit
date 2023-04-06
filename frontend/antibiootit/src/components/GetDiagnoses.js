5dfa7ba47025834047a58324a1f97b453453909e
Miia Honkanen <miia.honkanen@tuni.fi>
2023-04-06 12:01:14 +0300

    Add error handling to API fetches

--- frontend/antibiootit/src/components/GetDiagnoses.js
+++ frontend/antibiootit/src/components/GetDiagnoses.js
@@ -8,10 +8,30 @@ export default async function GetDiagnoses() {
             'X-API-KEY': apikey
         }
     }
-    return await fetch(url, options)
-        .then(res => res.json())
-        .then(data => {
-            return data.diagnosisInfos;
-        })
-        .catch(console.error)
+    try {
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+            if (response.status === 401) {
+                throw new Error("Invalid or missing API key");
+            } else if (response.status === 404) {
+                throw new Error("Resource not found");
+            } else if (response.status === 500) {
+                throw new Error("Internal server error");
+            } else {
+                throw new Error("Unknown error occurred");
+            }
+        }
+
+        const data = await response.json();
+
+        if (data.error) {
+            throw new Error(data.error.message);
+        }
+
+        return data.diagnosisInfos;
+    } catch (err) {
+        console.log(err);
+        throw new Error("An error occurred");
+    }
 }       
--- frontend/antibiootit/src/components/GetRecommendedTreatment.js
+++ frontend/antibiootit/src/components/GetRecommendedTreatment.js
@@ -1,4 +1,5 @@
 export default async function GetRecommendedTreatment(props) {
+
     const apikey = process.env.REACT_APP_API_KEY;
     const url = "https://backend-production-0993.up.railway.app/api/antibiotics/dose-calculation";
     const options = {
@@ -9,12 +10,33 @@ export default async function GetRecommendedTreatment(props) {
         },
         body: JSON.stringify(props)
     }
-    return await fetch(url, options)
-        .then(res => res.json() )
-        .then(data => {
-            return data;
-        })
-        .catch(err => {
-			console.log(err);
-		});
+    
+    try {
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+            if (response.status === 401) {
+                throw new Error("Invalid or missing API key");
+            } else if (response.status === 400) {
+                throw new Error("Invalid request parameters");
+            } else if (response.status === 404) {
+                throw new Error("Resource not found");
+            } else if (response.status === 500) {
+                throw new Error("Internal server error");
+            } else {
+                throw new Error("Unknown error occurred");
+            }
+        }
+
+        const data = await response.json();
+
+        if (data.error) {
+            throw new Error(data.error.message);
+        }
+
+        return data;
+    } catch (err) {
+        console.log(err);
+        throw new Error("An error occurred");
+    }
 }       
