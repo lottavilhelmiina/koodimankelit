48609ad657485a312fec593bbe5e814f701fdb96
Neera Kiviluoma <neeruska.kiviluoma@gmail.com>
2023-04-13 11:56:56 +0300

    check no antibiotics case with isEmpty

--- backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/builder/DiagnosisResponseBuilder.java
+++ backend/antibiootit/src/main/java/fi/tuni/koodimankelit/antibiootit/builder/DiagnosisResponseBuilder.java
@@ -25,7 +25,6 @@ public class DiagnosisResponseBuilder {
     private static int PRIMARY_CHOICE = 1;
     private static int SECONDARY_CHOICE = 2;
     private static int PENICILLIN_ALLERGIC_CHOICE = 3;
-    private static int NO_ANTIBIOTIC_CHOICE = 0;
 
     private Comparator<Strength> highestStrengthComparator = new Comparator<Strength>() {
 
@@ -110,6 +109,11 @@ public class DiagnosisResponseBuilder {
     private List<Treatment> getTreatments() {
 
         List<Treatment> treatments = new ArrayList<>();
+
+        if (this.diagnosis.getTreatments().isEmpty()) {
+            throw new NoAntibioticTreatmentException(this.diagnosis);
+        }
+
         for(Treatment treatment : this.diagnosis.getTreatments()) {
             if(isSuitableTreatment(treatment)) {
                 treatments.add(treatment);
@@ -132,11 +136,7 @@ public class DiagnosisResponseBuilder {
     private boolean isSuitableTreatment(Treatment treatment) {
         if(this.usePenicillinAllergic) {
             return PENICILLIN_ALLERGIC_CHOICE == treatment.getChoice();
-        }
-        else if (NO_ANTIBIOTIC_CHOICE == treatment.getChoice()) {
-            throw new NoAntibioticTreatmentException(this.diagnosis);
-        }
-        else {
+        } else {
             return PRIMARY_CHOICE == treatment.getChoice() || SECONDARY_CHOICE == treatment.getChoice();
         }
     }
